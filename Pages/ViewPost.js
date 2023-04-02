05a2b2e96b2a2be5af044493cebd2e5142ae931b
Aswin <aswinkrishna363@gmail.com>
2023-04-02 20:45:24 +0530

    adde the view post page

---
+++ Pages/ViewPost.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Components/Header/Header'
+import View from '../Components/View/View'
+
+export default function ViewPost(props) {
+    return (
+        <div>
+            <Header />
+            <View />
+        </div>
+    )
+}
