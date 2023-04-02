b1a79a44952de4de13f578406b8485fc9c4dce45
Aswin <aswinkrishna363@gmail.com>
2023-04-02 20:44:17 +0530

    added the selling page

---
+++ Pages/Create.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Header from '../Components/Header/Header';
+import Create from '../Components/Create/Create';
+
+export default function CreatePage() {
+  return (
+    <Fragment>
+      <Header />
+      <Create />
+    </Fragment>
+  );
+};
+
