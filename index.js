abc85cfedfd546adcc55f7df8bc24f5f26c7d198
Aswin <122090298+Aswin-786@users.noreply.github.com>
2023-04-29 11:11:18 +0530

    rearranged

--- index.js
+++ index.js
@@ -3,28 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-import { FirebaseContext, AuthContext } from './Store/Context'
+import { FirebaseContext } from './Store/Context'
 import Context from './Store/Context';
-
 import firebase from './firebase/config'
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  // <BrowserRouter>
+  <BrowserRouter>
     <FirebaseContext.Provider value={firebase}>
       <Context>
         <App />
       </Context>
     </FirebaseContext.Provider>
-  // </BrowserRouter>
-
+   </BrowserRouter>
 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
