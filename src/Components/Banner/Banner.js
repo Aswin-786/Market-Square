5aa1a89f975a1c050c1696f904f2507f2fc643ca
Aswin <122090298+Aswin-786@users.noreply.github.com>
2023-04-29 11:22:21 +0530

    updated medium screen visibility

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../../index.css'
 import './Banner.css';
 import Arrow from '../../assets/Arrow'
+
 function Banner() {
   return (
     <div className="pt-16">
@@ -11,14 +12,14 @@ function Banner() {
             <span>ALL CATEGORIES</span>
             <Arrow></Arrow>
           </div>
-          <div className=" md:flex ml-4 [&>*]:mr-4 sm:hidden">
+          <div className=" md:hidden ml-4 [&>*]:mr-4 sm:hidden lg:block">
             <span>Cars</span>
             <span>Motorcy...</span>
             <span>Mobile Ph...</span>
-            <span>For Sale:Houses & Apart...</span>
+            <span>For Sale:Houses</span>
             <span>Scoot...</span>
-            <span>Commercial & Other Ve...</span>
-            <span>For Rent: House & Apart...</span>
+            <span>vehic...</span>
+            <span>For Rent: House</span>
           </div>
         </div>
         <div className="md:relative md:w-full sm:relative">
@@ -32,4 +33,5 @@ function Banner() {
     </div>
   );
 }
+
 export default Banner;
