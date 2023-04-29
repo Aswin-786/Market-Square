438e6c36522a11d97f484fce91319a51ec5697f9
Aswin <122090298+Aswin-786@users.noreply.github.com>
2023-04-29 11:12:47 +0530

    modified

--- App.js
+++ App.js
@@ -1,30 +1,25 @@
-
 import React from 'react';
 import './App.css';
-
 import Signup from './Pages/Signup';
 import Login from './Pages/Login';
 import Home from './Pages/Home';
 import Create from './Pages/Create'
 import ViewPost from './Pages/ViewPost'
-
 import { BrowserRouter as Router, Route, } from 'react-router-dom'
-
-import { useState, useContext, useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import { AuthContext, FirebaseContext } from './Store/Context';
 import Post from './Store/PostContext';
 
 function App() {
+
   const { setUser } = useContext(AuthContext)
   const { firebase } = useContext(FirebaseContext)
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
-      console.log({ user });
       setUser(user)
     })
   })
-
 
   return (
     <>
@@ -52,18 +47,3 @@ function App() {
 }
 
 export default App;
-
-// import './App.css';
-// import {Firebase} from './firebase/config'
-// function App() {
-//   return (
-//     <div className="m-40">
-//       <button onClick={()=> {
-//         Firebase.firestore().collection('prducts').doc('Tab').delete()
-
-//       }}>Click</button>
-//     </div>
-//   );
-// }
-
-// export default App;
