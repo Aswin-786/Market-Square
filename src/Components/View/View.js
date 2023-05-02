898d1ded53fea858a8184a71f3e942ea372f6819
Aswin <122090298+Aswin-786@users.noreply.github.com>
2023-05-02 18:50:08 +0530

    updated message showing when no image uploaded

--- src/Components/View/View.js
+++ src/Components/View/View.js
@@ -1,86 +1,116 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import './Create.css';
+import Header from '../Header/Header';
 import { AuthContext, FirebaseContext } from '../../Store/Context';
-import { PostContext } from '../../Store/PostContext';
-import './View.css';
 import { useHistory } from 'react-router-dom';
 
-export default function View() {
+const Create = () => {
 
-  const [userDetails, setUserDetails] = React.useState()
-  // post context
-  const { PostDetails } = React.useContext(PostContext)
+  // accessing form inputs
+  const [name, setName] = React.useState('')
+  const [category, setCategory] = React.useState('')
+  const [price, setPrice] = React.useState('')
+  const [image, setImage] = React.useState(null)
+  const [error, setError] = React.useState(false)
+
   // firebase context
   const { firebase } = React.useContext(FirebaseContext)
+  // auth context
   const { user } = React.useContext(AuthContext)
-  const [remove, setRemove] = React.useState(false)
   // for redirectiong to pages
   const history = useHistory()
+  // present date
+  const date = new Date()
 
-  React.useEffect(() => {
+  //form submit
+  const handleUpload = () => {
 
-    // destructor user id
-    const { userId } = PostDetails
-    firebase.firestore().collection('users')
-      .where('id', '==', userId).get()
-      .then((res) => {
-        res.forEach((docs) => {
-          // setting to userDetails
-          setUserDetails(docs.data())
-
-          // if this is user own post
-          if (docs.data().id === user.uid) {
-            setRemove(true)
-          }
-        })
+    // if image null show message
+    if(image == null) {
+      console.log('no image');
+      setError(true)
+      return
+    }
+    // creating folder in firebase storage
+    firebase.storage().ref(`/image/${image.name}`)
+      // adding image to that folder
+      .put(image)
+      .then(({ ref }) => {
+        // taking the image url
+        ref.getDownloadURL()
+          .then((url) => {
+            console.log(url)
+            // adding details to products in collections
+            firebase.firestore().collection('products').add({
+              name,
+              category,
+              price,
+              url,
+              userId: user.uid,
+              createdAt: date.toDateString()
+            })
+            // redirecting to home page
+            history.push('/')
+          })
       })
-
-  }, [])
-
-  // delete the post
-  const handleDelete = () => {
-        firebase.firestore().collection('products').doc(PostDetails.id).delete().then(() => {
-      // redirecting to home page
-      history.push('/')
-    })
   }
-  
   return (
-    <div>
-      <div className="viewParentDiv">
-        <div className="imageShowDiv">
-          <img
-            src={PostDetails && PostDetails.url}
-            alt=""
-          />
+    <Fragment>
+      <Header />
+      <card>
+        <div className="centerDiv">
+          <form>
+            <label htmlFor="fname">Name</label>
+            <br />
+            <input
+              className="inputField"
+              type="text"
+              id="fname"
+              name="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+            <br />
+            <label htmlFor="fname">Category</label>
+            <br />
+            <input
+              className="inputField"
+              type="text"
+              id="fname"
+              name="category"
+              required
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            />
+            <br />
+            <label htmlFor="fname">Price</label>
+            <br />
+            <input
+              className="inputField"
+              type="number"
+              id="fname"
+              name="Price"
+              required
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+            <br />
+          </form>
+          <br />
+          <img alt="Posts" width="200px" height="200px" src={image ? URL.createObjectURL(image) : " "}></img>
+          <br />
+          <input
+            onChange={(e) => setImage(e.target.files[0])}
+            type="file"
+            required />
+          <br />
+          <button onClick={handleUpload} className="uploadBtn">upload and Submit</button>
+          { error && <span className='text-red-500 text-xs text-center py-3'> ** upload image **  </span>} 
         </div>
-        <div className="rightSection">
-          <div className="productDetails ">
-            <p>&#x20B9; {PostDetails && PostDetails.price} </p>
-            <span className='font-semibold'>{PostDetails && PostDetails.name}</span>
-            <p>{PostDetails && PostDetails.category}</p>
-            <span>{PostDetails && PostDetails.createdAt}</span>
-          </div>
-          <div className="contactDetails">
-            <p>Seller details</p>
-            <p>{userDetails && userDetails.userName}</p>
-            <p>{userDetails && userDetails.phone}</p>
-          </div>
-        </div>
-      </div>
-      <div className='delete'>
-        {
-          remove
-          &&
-          <botton
-            className='btn'
-            onClick={handleDelete}
-          >
-            Delete the Post
-          </botton>
-        }
-      </div>
-    </div>
-
+      </card>
+    </Fragment>
   );
+};
 
-}
+export default Create;
