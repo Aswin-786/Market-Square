b12b28151c32274eda7cf2b306e4d50149579068
Aswin <122090298+Aswin-786@users.noreply.github.com>
2023-05-06 22:56:40 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,13 +8,7 @@ import 'firebase/storage'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpbrk1LE2ko6iFB35Y72lHZNmuyR-rF_E",
-  authDomain: "olx-clone-91abd.firebaseapp.com",
-  projectId: "olx-clone-91abd",
-  storageBucket: "olx-clone-91abd.appspot.com",
-  messagingSenderId: "609388084156",
-  appId: "1:609388084156:web:906ac4bfe2a229fae549b9",
-  measurementId: "G-C5BVL8W49X"
+  
 };
 
 export default firebase.initializeApp(firebaseConfig)
